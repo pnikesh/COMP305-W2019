9a6fb5886404a5026ee331dbe31f69ef6f4480d8
Nik <nikeshpatel356@gmail.com>
2019-03-27 19:47:41 -0400

    Initialized item counter UI

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public UI_CherryCounter ui_cherryCounter;
+    private int itemCounter = 0;  //how many items picked up
+    private int totalItemCount = 0;  //how many item are in my level
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalItemCount = GameObject.FindGameObjectsWithTag("Item").Length;
+        ui_cherryCounter.UpdateUI(itemCounter, totalItemCount);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
