b8ddf5976cd05fce95496dcfc9fb91947a5cb383
Nik <nikeshpatel356@gmail.com>
2019-04-03 20:18:06 -0400

    Game can be paused with sound also paused and removed unwanted files

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class GameController : MonoBehaviour
 {
+    [Header("User Interface")]
     public UI_CherryCounter ui_cherryCounter;
     public GameObject ui_EndLevelPanel;
+
+    [Header("Audio")]
+    public AudioMixerSnapshot paused;
+    public AudioMixerSnapshot unpaused;
     private int itemCounter = 0;  //how many items picked up
     private int totalItemCount = 0;  //how many item are in my level
 
@@ -18,6 +24,24 @@ public class GameController : MonoBehaviour
         ui_cherryCounter.UpdateUI(itemCounter, totalItemCount);
 
 
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            //set audo snapshot to pause or unpause snapshot
+            //pause the game
+            Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+
+            if(Time.timeScale == 0) //paused
+            {
+                paused.TransitionTo(0.01f);
+            }
+            else //unpaused
+            {
+                unpaused.TransitionTo(0.01f);
+            }
+        }
     }
 
     //called when item is picked up
